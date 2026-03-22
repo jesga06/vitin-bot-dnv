174ead807ba948576cc3ef636859509b8b9c66ac
073vitin <vsena3231@gmail.com>
2026-03-22 20:07:42 -0300

    Delete routers/utils.js

--- routers/utils.js
+++
@@ -1,6 +0,0 @@
-function isAdmin(user, group) {
-  const admins = ['5511999999999@s.whatsapp.net', '5511888888888@s.whatsapp.net']; // insira seus admins
-  return admins.includes(user);
-}
-
-module.exports = { isAdmin };
